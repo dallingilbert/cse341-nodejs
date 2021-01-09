1e962060ab1d01e1faaf78df3b6720bbd104c768
Dallin Gilbert <djgilb24@byui.edu>
2021-01-08 17:30:53 -0700

    adding more files

---
+++ cse341-project/routes/prove01.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const fs = require('fs'); // File system for PROVE01
+const router = express.Router();
+
+router.get('/',(req, res, next) => {
+    res.render('pages/twoInputs', { 
+        title: 'Two Inputs', 
+        path: '/prove01', // For pug, EJS 
+        activeTA03: true, // For HBS
+        contentCSS: true, // For HBS
+    });
+    return res.end();
+});
+
+router.get('/inputDisplay', (req, res) => {
+    res.render('pages/twoInputDisplay', { 
+        title: 'Two Input Display', 
+        path: '/prove01', // For pug, EJS 
+        activeTA03: true, // For HBS
+        contentCSS: true, // For HBS
+    });
+});
+
+module.exports = router;

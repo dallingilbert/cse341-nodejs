60b0b5ae94fcc786ea5899aebc165857103ea665
Dallin Gilbert <djgilb24@byui.edu>
2021-01-13 15:35:23 -0700

    modifying admin and shop js files

--- workingExamples/app/app.js
+++ workingExamples/app/app.js
@@ -3,20 +3,13 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use( '/add-product', (req, res, next) => {
-    console.log("In another middleware");
-    res.send('<html><form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</form></html>');
-});
+app.use(adminRoutes);
 
-app.post('/product', (req, res, next) => {
-    console.log(req.body);
-    res.redirect('/');
-});
-
-app.use( '/', (req, res, next) => {
-    res.send('<h1>Hello from Express</h1>');
-});
+app.use(shopRoutes);
 
 app.listen(3000);

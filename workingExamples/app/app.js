4175f5cb654900509d782f6292b240dd6669e502
Dallin Gilbert <djgilb24@byui.edu>
2021-01-13 22:07:03 -0700

    adding functionality to routes files

--- workingExamples/app/app.js
+++ workingExamples/app/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const app = express();
 
@@ -8,8 +9,12 @@ const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use(adminRoutes);
+app.use('/admin', adminRoutes);
 
 app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', 'error-404.html'));
+});
 
 app.listen(3000);
--- workingExamples/app/routes/admin.js
+++ workingExamples/app/routes/admin.js
@@ -1,14 +1,17 @@
+const path = require('path');
+
 const express = require('express');
 
 const router = express.Router();
  
-
+// /admin/add-product => GET
 router.get( '/add-product', (req, res, next) => {
     console.log("In another middleware");
-    res.send('<html><form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</form></html>');
+    res.sendFile(path.join(__dirname, '..', 'views', 'add-product.html'));
 });
 
-router.post('/product', (req, res, next) => {
+// /admin/add-product => POST
+router.post('/add-product', (req, res, next) => {
     console.log(req.body);
     res.redirect('/');
 });
